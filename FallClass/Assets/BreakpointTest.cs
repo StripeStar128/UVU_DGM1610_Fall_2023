e54981a0338a40a7db4ee3c9d27f058637c18aa8
Julia Fullmer <stripestar128128@gmail.com>
2023-09-27 19:33:07 -0600

    Added Breakpoint test

---
+++ FallClass/Assets/BreakpointTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakpointTest : MonoBehaviour
+{
+    //Use this for initialization
+    void Start()
+    {
+        Debug.Log("Debug message here");
+    }
+}
