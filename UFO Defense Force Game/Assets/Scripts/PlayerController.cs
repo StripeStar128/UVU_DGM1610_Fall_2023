8381c55670f9b819db2e6782dfd11219777f8a1a
Julia Fullmer <stripestar128128@gmail.com>
2023-10-04 17:38:37 -0600

    Edited Player Controller

--- UFO Defense Force Game/Assets/Scripts/PlayerController.cs
+++ UFO Defense Force Game/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed;
+
+    public float xRange;
     
     
     // Start is called before the first frame update
@@ -17,10 +19,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Set horizontalInput to recieve values from keyboard
+        // Set horizontalInput to receive values from keyboard
         horizontalInput = Input.GetAxis("Horizontal");
         
         // Moves Player left and right
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        // Keep player within bounds
+        //Left side wall
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        // Right side wall
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
     }
 }
