bb3c7acf6891f07b4ee36914f58d442277496592
Julia Fullmer <stripestar128128@gmail.com>
2023-10-16 22:25:52 -0600

    Added Score Manager

--- UFO Defense Force Game/Assets/Scripts/CollisionDetect.cs
+++ UFO Defense Force Game/Assets/Scripts/CollisionDetect.cs
@@ -2,11 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class CollisionDetect : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    public ScoreManager scoreManager; // Store reference to score manager
+
+    public int scoreToGive;
+
+    void Start()
     {
+        scoreManager = GameObject.Find('ScoreManager').GetComponent<ScoreManager>(); // Find ScoreManager gameobject and reference ScoreManager script component
+    }
+    void OnTriggerEnter(Collider other) // Once the Trigger has been entered record collision in the argument variable "other"
+    {
+        scoreManager.IncreaseScore(scoreToGive); // Increase the Score
         Destroy(gameObject); // Destroy this gameobject
         Destroy(other.gameObject); // Destroys the other gameobject it hits
     }
