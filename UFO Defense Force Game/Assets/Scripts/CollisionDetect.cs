3918cba3990614df6b514befb999c51b016ce548
Julia Fullmer <stripestar128128@gmail.com>
2023-10-19 19:43:55 -0600

    Edited Audio

--- UFO Defense Force Game/Assets/Scripts/CollisionDetect.cs
+++ UFO Defense Force Game/Assets/Scripts/CollisionDetect.cs
@@ -8,15 +8,19 @@ public class CollisionDetect : MonoBehaviour
     public ScoreManager scoreManager; // Store reference to score manager
 
     public int scoreToGive;
-
+    public AudioClip explosion;
+    private AudioSource enemyAudio;
     void Start()
     {
         scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>(); // Find ScoreManager gameObject and reference ScoreManager script component
+        enemyAudio = GetComponent<AudioSource>();
     }
     void OnTriggerEnter(Collider other) // Once the Trigger has been entered record collision in the argument variable "other"
     {
         scoreManager.IncreaseScore(scoreToGive); // Increase the Score
+        enemyAudio.PlayOneShot(explosion, 1.0f);
         Destroy(gameObject); // Destroy this gameObject
         Destroy(other.gameObject); // Destroys the other gameObject it hits
+        
     }
 }
--- UFO Defense Force Game/Assets/Scripts/MainMenu.cs
+++ UFO Defense Force Game/Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,24 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public int sceneToLoad;
-    
+    public AudioClip buttonSound;
+    private AudioSource menuAudio;
+
+    private void Start()
+    {
+        menuAudio = GetComponent<AudioSource>();
+    }
+
     public void StartGame()
     {
+        menuAudio.PlayOneShot(buttonSound, 1.0f);
         SceneManager.LoadScene(sceneToLoad); // Scene to Load
         Debug.Log("New Scene Loaded!");
     }
 
     public void QuitGame()
     {
+        menuAudio.PlayOneShot(buttonSound, 1.0f);
         Application.Quit();
         Debug.Log("Quit Game!");
     }
