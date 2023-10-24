c59eabd9460f1c8bf4719ac9cb1d8ffe08e8c9b4
Julia Fullmer <stripestar128128@gmail.com>
2023-10-23 20:07:16 -0600

    Added Scriptable Objects

---
+++ ScriptableObjects/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed;
+    public float xRange;
+    void Update()
+    {
+        // Set horizontalInput to receive values from keyboard
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+        // Moves Player left and right
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        // Keep player within bounds
+        //Left side wall
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        // Right side wall
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
