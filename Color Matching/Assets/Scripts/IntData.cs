9d9f93d54d60f659aa7ac7adb82dcb09585cb5eb
Julia Fullmer <stripestar128128@gmail.com>
2023-11-07 18:06:23 -0700

    Added Audio

--- Color Matching/Assets/Scripts/IntData.cs
+++ Color Matching/Assets/Scripts/IntData.cs
@@ -1,17 +1,42 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu]
 public class IntData : ScriptableObject
 {
-    public int value; 
+    public int value;
     
+    //public UnityEvent disableEvent;
     public void SetValue(int num)
     {
         value = num;
     }
-    
+
+    public void CompareValue(IntData obj)
+    {
+        if (value >= obj.value)
+        {
+            
+        }
+        else
+        {
+            value = obj.value;
+        }
+    }
+
+    public void SetValue(IntData obj)
+    {
+        value = obj.value;
+    }
+
     public void UpdateValue(int num)
     {
         value += num;
     }
+
+    /*private void OnDisable()
+    {
+        disableEvent.Invoke();
+    }*/
 }
