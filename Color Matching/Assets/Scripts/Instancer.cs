54108b2c66c52dc558695b372a212903590db662
Julia Fullmer <stripestar128128@gmail.com>
2023-11-15 22:28:47 -0700

    Added Instances

--- Color Matching/Assets/Scripts/Instancer.cs
+++ Color Matching/Assets/Scripts/Instancer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Instancer : ScriptableObject
 {
     public GameObject prefab;
+    public int num;
     
     public void CreateInstance()
     {
@@ -13,5 +14,29 @@ public class Instancer : ScriptableObject
     public void CreateInstance(Vector3Data obj)
     {
         Instantiate(prefab, obj.value, Quaternion.identity);
+    }
+
+    public void CreateInstanceFromList(Vector3DataList obj)
+    {
+        foreach (var t in obj.vector3DList)
+        {
+            Instantiate(prefab, t.value, Quaternion.identity);
+        }
+    }
+    
+    public void CreateInstanceFromListCounting(Vector3DataList obj)
+    {
+        Instantiate(prefab, obj.vector3DList[num].value, Quaternion.identity);
+        num++;
+        if (num == obj.vector3DList.Count)
+        {
+            num = 0;
+        }
+    }
+    
+    public void CreateInstanceFromListRandomly(Vector3DataList obj)
+    {
+        num = Random.Range(0, obj.vector3DList.Count - 1);
+        Instantiate(prefab, obj.vector3DList[num].value, Quaternion.identity);
     }
 }
