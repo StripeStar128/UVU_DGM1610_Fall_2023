d75248dde17c005b2e59f3fe57b6fda65e44d540
Julia Fullmer <stripestar128128@gmail.com>
2023-11-15 21:27:36 -0700

    Added Coroutines

---
+++ Color Matching/Assets/Scripts/Instancer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Instancer : ScriptableObject
+{
+    public GameObject prefab;
+    
+    public void CreateInstance()
+    {
+        Instantiate(prefab);
+    }
+
+    public void CreateInstance(Vector3Data obj)
+    {
+        Instantiate(prefab, obj.value, Quaternion.identity);
+    }
+}
